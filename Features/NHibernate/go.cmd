c787794ffafb7634244cb92942c8ebce44f11f83
Rinat.Abdullin <>
2010-06-21 11:33:33 +0200

    Cleanup

--- Features/NHibernate/go.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-@%SystemRoot%\Microsoft.NET\Framework\v3.5\msbuild Project.build %*
--- Source/Lokad.Cqrs/Scheduled/IScheduledTaskBuilder.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Lokad.Cqrs.Scheduled
-{
-	public interface IScheduledTaskBuilder
-	{
-		IEnumerable<ScheduledTaskInfo> BuildTasks();
-	}
-}
