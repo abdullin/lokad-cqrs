952ed39159e3e3a0125958498c59c807f67f8933
Rinat.Abdullin <>
2010-06-23 10:55:43 +0200

    Framework: rewrite of NHibernate Module to be more consistent with the overall design.

--- Framework/Source/Lokad.Cqrs.NHibernate/NHibernateModule.cs
+++ Framework/Source/Lokad.Cqrs.NHibernate/NHibernateModule.cs
@@ -7,6 +7,7 @@
 
 using System;
 using Autofac;
+using Autofac.Core;
 using Lokad.Settings;
 using NHibernate;
 using NHibernate.Bytecode;
@@ -15,26 +16,21 @@ using NHibernate.Cfg;
 
 namespace Lokad.Cqrs.NHibernate
 {
-	public sealed class NHibernateModule : Module
+	public sealed class NHibernateModule : IModule
 	{
-		Action<ContainerBuilder> _registrations = builder => { };
+		readonly ContainerBuilder _builder = new ContainerBuilder();
 
 		public NHibernateModule()
 		{
 			WithProxyFactory<ProxyFactoryFactory>();
 		}
-
-
-		protected override void Load(ContainerBuilder builder)
-		{
-			builder.Register(c => c.Resolve<Configuration>().BuildSessionFactory()).SingleInstance();
-			builder.Register(ComposeSession).InstancePerLifetimeScope();
-			builder.RegisterType<NHibernateStarter>().As<IStartable>();
-		}
-
-
 		string _proxyFactoryClass;
 
+		/// <summary>
+		/// Specifies custom proxy factory to use
+		/// </summary>
+		/// <typeparam name="TFactory">The type of the factory.</typeparam>
+		/// <returns>same module instance for inlining in fluent configs</returns>
 		public NHibernateModule WithProxyFactory<TFactory>()
 			where TFactory : IProxyFactoryFactory
 		{
@@ -42,10 +38,15 @@ namespace Lokad.Cqrs.NHibernate
 			return this;
 		}
 
+		/// <summary>
+		/// Configures module to use NHibernate with the provided configuration
+		/// </summary>
+		/// <param name="config">The config.</param>
+		/// <returns>same module instance for inlining in fluent configs</returns>
 		public NHibernateModule WithConfiguration(Configuration config)
 		{
 			ApplyProxyFactory(config);
-			_registrations += builder => builder.RegisterInstance(config);
+			_builder.RegisterInstance(config);
 			return this;
 		}
 
@@ -69,11 +70,17 @@ namespace Lokad.Cqrs.NHibernate
 				instance.Properties["proxyfactory.factory_class"] = _proxyFactoryClass;
 			}
 		}
-
-		public NHibernateModule WithConfiguration(string connection, Func<string, Configuration> config)
+		/// <summary>
+		///Configures module to use NHibernate with the connection string looked up by the key
+		/// from the application configs
+		/// </summary>
+		/// <param name="connectionKeyName">The connection key name to use.</param>
+		/// <param name="configFactory">The configuration Factoru.</param>
+		/// <returns>same module for usage in fluent configurations</returns>
+		public NHibernateModule WithConfiguration(string connectionKeyName, Func<string, Configuration> configFactory)
 		{
-			_registrations += builder => builder
-				.Register(c => ComposeConfiguration(c, connection, config))
+			_builder
+				.Register(c => ComposeConfiguration(c, connectionKeyName, configFactory))
 				.SingleInstance();
 			return this;
 		}
@@ -82,8 +89,15 @@ namespace Lokad.Cqrs.NHibernate
 		{
 			var session = c.Resolve<ISessionFactory>().OpenSession();
 			session.FlushMode = FlushMode.Commit;
-
 			return session;
+		}
+
+		void IModule.Configure(IComponentRegistry componentRegistry)
+		{
+			_builder.Register(c => c.Resolve<Configuration>().BuildSessionFactory()).SingleInstance();
+			_builder.Register(ComposeSession).InstancePerLifetimeScope();
+			_builder.RegisterType<NHibernateStarter>().As<IStartable>();
+			_builder.Update(componentRegistry);
 		}
 	}
 }
