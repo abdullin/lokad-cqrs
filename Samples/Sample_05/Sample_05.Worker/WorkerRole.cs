7803eba41d75da87827ecdf2f073cf24d19276a9
rinat.abdullin <none@none>
2010-09-16 17:04:55 +0600

    Initial files for the Sample 05.
    --HG--
    branch : 2010-09-19_Sample_05

---
+++ Samples/Sample_05/Sample_05.Worker/WorkerRole.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Diagnostics;
+using Microsoft.WindowsAzure.ServiceRuntime;
+using Microsoft.WindowsAzure.StorageClient;
+
+namespace Sample_05.Worker
+{
+	public class WorkerRole : RoleEntryPoint
+	{
+		public override void Run()
+		{
+			// This is a sample worker implementation. Replace with your logic.
+			Trace.WriteLine("Sample_05.Worker entry point called", "Information");
+
+			while (true)
+			{
+				Thread.Sleep(10000);
+				Trace.WriteLine("Working", "Information");
+			}
+		}
+
+		public override bool OnStart()
+		{
+			// Set the maximum number of concurrent connections 
+			ServicePointManager.DefaultConnectionLimit = 12;
+
+			DiagnosticMonitor.Start("DiagnosticsConnectionString");
+
+			// For information on handling configuration changes
+			// see the MSDN topic at http://go.microsoft.com/fwlink/?LinkId=166357.
+			RoleEnvironment.Changing += RoleEnvironmentChanging;
+
+			return base.OnStart();
+		}
+
+		private void RoleEnvironmentChanging(object sender, RoleEnvironmentChangingEventArgs e)
+		{
+			// If a configuration setting is changing
+			if (e.Changes.Any(change => change is RoleEnvironmentConfigurationSettingChange))
+			{
+				// Set e.Cancel to true to restart this role instance
+				e.Cancel = true;
+			}
+		}
+	}
+}
