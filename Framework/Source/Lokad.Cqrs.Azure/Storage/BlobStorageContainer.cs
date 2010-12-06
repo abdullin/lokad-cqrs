8217ce8ccd16190b5b4efb79b58334adac980796
rinat.abdullin <none@none>
2010-12-06 16:39:34 +0500

    Cosmetics

--- Framework/Source/Lokad.Cqrs.Azure/Storage/BlobStorageContainer.cs
+++ Framework/Source/Lokad.Cqrs.Azure/Storage/BlobStorageContainer.cs
@@ -13,31 +13,6 @@ namespace Lokad.Cqrs.Storage
 	/// <summary>
 	/// Windows Azure implementation of storage 
 	/// </summary>
-	public sealed class BlobStorageRoot : IStorageRoot
-	{
-		readonly CloudBlobClient _client;
-		readonly ILogProvider _provider;
-
-		/// <summary>
-		/// Initializes a new instance of the <see cref="BlobStorageRoot"/> class.
-		/// </summary>
-		/// <param name="client">The client.</param>
-		/// <param name="provider">The provider.</param>
-		public BlobStorageRoot(CloudBlobClient client, ILogProvider provider)
-		{
-			_client = client;
-			_provider = provider;
-		}
-
-		public IStorageContainer GetContainer(string name)
-		{
-			return new BlobStorageContainer(_client.GetBlobDirectoryReference(name));
-		}
-	}
-
-	/// <summary>
-	/// Windows Azure implementation of storage 
-	/// </summary>
 	public sealed class BlobStorageContainer : IStorageContainer
 	{
 		readonly CloudBlobDirectory _directory;
