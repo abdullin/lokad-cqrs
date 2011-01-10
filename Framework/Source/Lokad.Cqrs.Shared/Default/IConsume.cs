9635ddf8f5cbe95a93cbf7233242e385f4ba6f5c
rinat.abdullin <none@none>
2011-01-10 13:54:16 +0500

    Added comments to IMessage and IConsumeMessage.

--- Framework/Source/Lokad.Cqrs.Shared/Default/IConsume.cs
+++ Framework/Source/Lokad.Cqrs.Shared/Default/IConsume.cs
@@ -8,8 +8,13 @@
 namespace Lokad.Default
 {
 	/// <summary>
-	/// Default CQRS interface for interface-base domain setup
+	/// <para>Default CQRS interface for interface-base domain setup of message consumers. By default Lokad.CQRS 
+	/// scans user assemblies for message handlers inheriting from this interface.</para>
+	/// <para>If you don't want to reference Lokad.CQRS assemblies in your domain, 
+	/// you can declare your own consumer interface and point to it in the configuration,
+	/// as shown in the samples.</para>
 	/// </summary>
+	/// <remarks>Look in the samples for more details on the usage</remarks>
 	public interface IConsume<in TMessage> : IConsumeMessage
 		where TMessage : IMessage
 	{
--- Framework/Source/Lokad.Cqrs.Shared/Default/IMessage.cs
+++ Framework/Source/Lokad.Cqrs.Shared/Default/IMessage.cs
@@ -8,8 +8,13 @@
 namespace Lokad.Default
 {
 	/// <summary>
-	/// Default CQRS message interface for the domain setup
+	/// <para>Default CQRS message interface for the domain setup. By default Lokad.CQRS 
+	/// scans assemblies for message contracts deriving from this interface.</para>
+	/// <para>If you don't want to reference Lokad.CQRS assemblies in your domain, 
+	/// you can declare your own message interface and point to it in the configuration,
+	/// as shown in the samples.</para>
 	/// </summary>
+	/// <remarks>Look in the samples for more details on the usage</remarks>
 	public interface IMessage
 	{
 	}
