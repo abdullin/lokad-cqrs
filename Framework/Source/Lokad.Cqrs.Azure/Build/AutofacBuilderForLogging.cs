8993694696fee23cf24193883781d8f9abcffc0f
rinat.abdullin <none@none>
2011-02-08 18:39:58 +0100

    Dropping log provider extensions.
    --HG--
    branch : 2011-02-08-simplification

--- Framework/Source/Lokad.Cqrs.Azure/Build/AutofacBuilderForLogging.cs
+++ Framework/Source/Lokad.Cqrs.Azure/Build/AutofacBuilderForLogging.cs
@@ -2,13 +2,23 @@
 
 namespace Lokad.Cqrs
 {
-	public sealed class AutofacBuilderForLogging : Syntax, ISupportSyntaxForLogging
+	public sealed class AutofacBuilderForLogging : Syntax
 	{
 		readonly ContainerBuilder _builder;
 
 		public AutofacBuilderForLogging(ContainerBuilder builder)
 		{
 			_builder = builder;
+		}
+
+		public void LogToNull()
+		{
+			RegisterLogProvider(NullLog.Provider);
+		}
+
+		public void LogToTrace()
+		{
+			RegisterLogProvider(TraceLog.Provider);
 		}
 
 		public void RegisterLogProvider(ILogProvider provider)
