b4b0037afb7502d52b20407d04dd193be40be41d
rinat.abdullin <none@none>
2011-02-08 17:36:02 +0100

    Removed separate Lokad.Cqrs.Storage project.
    --HG--
    branch : 2011-02-08-simplification

--- Framework/Source/Lokad.Cqrs.Core/Serialization/ProtoBufMessageSerializer.cs
+++ Framework/Source/Lokad.Cqrs.Core/Serialization/ProtoBufMessageSerializer.cs
@@ -11,7 +11,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.Serialization;
-using Lokad.Serialization;
+
 using ExtendIEnumerable = Lokad.ExtendIEnumerable;
 
 namespace Lokad.Cqrs.Serialization
