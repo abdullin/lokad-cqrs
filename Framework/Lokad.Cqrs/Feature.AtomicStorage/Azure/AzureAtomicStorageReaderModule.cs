7e81aa7a054592c8e53e1f9578da96665b625fa9
Rinat Abdullin <rinat.abdullin@gmail.com>
2011-04-13 19:02:06 +0600

    Added atomic storage reader module (used on the clients).
    --HG--
    branch : 2011-04-13-importing-atomic-state

---
+++ Framework/Lokad.Cqrs/Feature.AtomicStorage/Azure/AzureAtomicStorageReaderModule.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+
+namespace Lokad.Cqrs.Feature.AtomicStorage.Azure
+{
+	public sealed class AzureAtomicStorageReaderModule : Module
+	{
+		readonly IAzureAtomicStorageStrategy _strategy;
+
+		public AzureAtomicStorageReaderModule(IAzureAtomicStorageStrategy strategy)
+		{
+			_strategy = strategy;
+		}
+
+
+		protected override void Load(ContainerBuilder builder)
+		{
+			builder
+				.RegisterGeneric(typeof(AzureAtomicEntityReader<,>))
+				.As(typeof(IAtomicEntityReader<,>))
+				.SingleInstance();
+			builder
+				.RegisterGeneric(typeof(AzureAtomicSingletonReader<>))
+				.As(typeof(IAtomicSingletonReader<>))
+				.SingleInstance();
+
+			builder.RegisterInstance(_strategy);
+		}
+	}
+}
