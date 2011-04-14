4f97f5da01313a544920e76e9a1c82ddee33484c
Rinat Abdullin <rinat.abdullin@gmail.com>
2011-04-14 20:02:32 +0600

    Cosmetics
    --HG--
    branch : 2011-04-14-queue-notifiers

--- Framework/Lokad.Cqrs/Feature.TestTransport/MemoryPartitionSchedulerFactory.cs
+++ Framework/Lokad.Cqrs/Feature.TestTransport/MemoryPartitionSchedulerFactory.cs
@@ -1,14 +1,21 @@
-﻿using System;
+﻿#region (c) 2010-2011 Lokad - CQRS for Windows Azure - New BSD License 
+
+// Copyright (c) Lokad 2010-2011, http://www.lokad.com
+// This code is released as Open Source under the terms of the New BSD Licence
+
+#endregion
+
 using System.Collections.Concurrent;
-using System.Collections.Generic;
-using Lokad.Cqrs.Core.Transport;
 using System.Linq;
+using Lokad.Cqrs.Core.Transport;
 
 namespace Lokad.Cqrs.Feature.TestTransport
 {
 	public sealed class MemoryPartitionSchedulerFactory : IPartitionSchedulerFactory, IWriteQueueFactory
 	{
-		readonly ConcurrentDictionary<string, BlockingCollection<MessageEnvelope>> _factory = new ConcurrentDictionary<string, BlockingCollection<MessageEnvelope>>();
+		readonly ConcurrentDictionary<string, BlockingCollection<MessageEnvelope>> _factory =
+			new ConcurrentDictionary<string, BlockingCollection<MessageEnvelope>>();
+
 
 		public IWriteQueue GetWriteQueue(string name)
 		{
