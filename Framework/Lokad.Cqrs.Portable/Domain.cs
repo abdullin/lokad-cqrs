21d04a1acafddbe8271034bb0d6221238c625e46
Rinat Abdullin <rinat.abdullin@gmail.com>
2011-04-26 20:25:16 +0600

    Default domain implementation

---
+++ Framework/Lokad.Cqrs.Portable/Domain.cs
@@ -0,0 +1,29 @@
+﻿using Lokad.Cqrs.Core.Directory.Default;
+
+namespace Lokad.Cqrs
+{
+    /// <summary>
+    /// Default implementations of the domain-specific interfaces
+    /// </summary>
+    public static class Domain
+    {
+        public interface Event : IMessage
+        {
+
+        }
+        public interface Command : IMessage
+        {
+
+        }
+
+        public interface Consumer<TEvent> : IConsume<TEvent> where TEvent : Event
+        {
+            
+        }
+
+        public interface Handler<TCommand> : IConsume<TCommand> where TCommand : Command
+        {
+            
+        }
+    }
+}
