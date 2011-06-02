b4dd5b0690b590bc10a320f4e1f09c2ce6832405
Rinat Abdullin <rinat.abdullin@gmail.com>
2011-06-02 12:21:55 +0200

    Core interfaces for the tape storage (event persistence).
    --HG--
    branch : 2011-05-28-es

---
+++ Framework/Lokad.Cqrs.Portable/Feature.TapeStorage/ITapeReader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Lokad.Cqrs.Feature.TapeStorage
+{
+    public interface ITapeReader
+    {
+        /// <summary>
+        /// Reads up to <see cref="maxCount"/> records starting from the <see cref="index"/>.
+        /// </summary>
+        /// <param name="index">The index to start reading from.</param>
+        /// <param name="maxCount">The max count of records to load.</param>
+        /// <returns>collection of taped blocks</returns>
+        IEnumerable<TapeRecord> ReadRecords(int index, int maxCount);
+    }
+}
---
+++ Framework/Lokad.Cqrs.Portable/Feature.TapeStorage/TapeRecord.cs
@@ -0,0 +1,17 @@
+﻿namespace Lokad.Cqrs.Feature.TapeStorage
+{
+    /// <summary>
+    /// Contains information about the committed data
+    /// </summary>
+    public sealed class TapeRecord
+    {
+        public readonly int Index;
+        public readonly byte[] Data;
+
+        public TapeRecord(int index, byte[] data)
+        {
+            Index = index;
+            Data = data;
+        }
+    }
+}
