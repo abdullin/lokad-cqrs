7b710847612a3e9940ee128f4e5ad1405ed8f223
Rinat Abdullin <rinat.abdullin@gmail.com>
2011-06-17 00:06:17 +0600

    Comments for IMessageDispatchScope.cs

--- Framework/Lokad.Cqrs.Portable/Core.Dispatch/IMessageDispatchScope.cs
+++ Framework/Lokad.Cqrs.Portable/Core.Dispatch/IMessageDispatchScope.cs
@@ -2,9 +2,21 @@
 
 namespace Lokad.Cqrs.Core.Dispatch
 {
+    /// <summary>
+    /// Logical transaction and resolution hierarchy for dispatching this specific message.
+    /// </summary>
     public interface IMessageDispatchScope : IDisposable
     {
-        void Dispatch(Type consumerType, ImmutableEnvelope envelop, ImmutableMessage message);
+        /// <summary>
+        /// Dispatches the specified message to instance of the consumer type.
+        /// </summary>
+        /// <param name="consumerType">Type of the consumer expected.</param>
+        /// <param name="envelope">The envelope context.</param>
+        /// <param name="message">The actual message to dispatch.</param>
+        void Dispatch(Type consumerType, ImmutableEnvelope envelope, ImmutableMessage message);
+        /// <summary>
+        /// Completes this scope.
+        /// </summary>
         void Complete();
     }
 }
