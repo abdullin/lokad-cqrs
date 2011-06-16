9968d9cadbcd88b0b15ea1ef6803a33ef74ea013
Rinat Abdullin <rinat.abdullin@gmail.com>
2011-06-16 11:57:10 +0600

    Added documentation to IEnvelopeQuarantine.cs

--- Framework/Lokad.Cqrs.Portable/Core.Dispatch/IEnvelopeQuarantine.cs
+++ Framework/Lokad.Cqrs.Portable/Core.Dispatch/IEnvelopeQuarantine.cs
@@ -10,9 +10,28 @@ using Lokad.Cqrs.Core.Inbox;
 
 namespace Lokad.Cqrs.Core.Dispatch
 {
+    /// <summary>
+    /// Implements quarantine logic for the specific message handler. Default implementation 
+    /// is <see cref="MemoryQuarantine"/>
+    /// </summary>
     public interface IEnvelopeQuarantine
     {
+        /// <summary>
+        /// Tries to quarantine the specified envelope. Implementation can decide whether we need to give another
+        /// try to process the envelope (by returning <em>False</em>) or if quarantine should accept the envelope
+        /// completely. Then processor will discard the queue from it's incoming queue and leave it up to the
+        /// quarantine to record it, push to poison etc. 
+        /// </summary>
+        /// <param name="context">The envelope transport context.</param>
+        /// <param name="ex">The exception.</param>
+        /// <returns><em>True</em> if envelope should be quarantined right away (i.e. exception happened 4 times)
+        /// and is not excepted to be processed by the queue any more; <em>False</em> otherwise</returns>
         bool TryToQuarantine(EnvelopeTransportContext context, Exception ex);
+        /// <summary>
+        /// Tries to release envelope record from the partial or full quarantine (I.e.: when message
+        /// has been successfully processed and quarantine can forget about it forever).
+        /// </summary>
+        /// <param name="context">The envelope transport context.</param>
         void TryRelease(EnvelopeTransportContext context);
     }
 }
