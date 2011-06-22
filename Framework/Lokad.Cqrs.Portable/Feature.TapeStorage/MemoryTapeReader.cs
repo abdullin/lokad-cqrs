529d0f22a39b305c883357dbc897cf102a189438
Rinat Abdullin <rinat.abdullin@gmail.com>
2011-06-22 18:52:33 +0600

    Added memory implementation of the tape storage.
    --HG--
    branch : 2011-05-28-es

---
+++ Framework/Lokad.Cqrs.Portable/Feature.TapeStorage/MemoryTapeReader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lokad.Cqrs.Feature.TapeStorage
+{
+    public sealed class MemoryTapeReader : ITapeReader
+    {
+        readonly Func<byte[][]> _getSnapshot;
+        public MemoryTapeReader(Func<byte[][]> getSnapshot)
+        {
+            _getSnapshot = getSnapshot;
+        }
+
+        public IEnumerable<TapeRecord> ReadRecords(long index, int maxCount)
+        {
+            return _getSnapshot()
+                .Select((b,i) => new TapeRecord(i+1, b))
+                .Where(tr => index>=tr.Index)
+                .Take(maxCount)
+                .ToArray();
+        }
+    }
+}
