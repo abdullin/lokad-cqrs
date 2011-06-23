bb51501cc08809d0d65ecea61875f26bcc133ec0
Alexandr Zaozerskiy <zaozerskyau@gmail.com>
2011-06-24 00:13:17 +0600

    Updated sample 03 to the latest Lokad.CQRS

--- Samples/Sample_03/Sample_03.Worker/StringUtil.cs
+++ Samples/Sample_03/Sample_03.Worker/StringUtil.cs
@@ -1,8 +1,7 @@
-﻿#region Copyright (c) 2010 Lokad. New BSD License
+﻿#region (c) 2010-2011 Lokad. New BSD License
 
-// Copyright (c) Lokad 2010 SAS 
-// Company: http://www.lokad.com
-// This code is released as Open Source under the terms of the New BSD licence
+// Copyright (c) Lokad 2010-2011, http://www.lokad.com
+// This code is released as Open Source under the terms of the New BSD Licence
 
 #endregion
 
@@ -10,11 +9,11 @@ using System;
 
 namespace Sample_03.Worker
 {
-	static class StringUtil
-	{
-		public static string ToReadable(this Guid guid)
-		{
-			return guid.ToString().Substring(0, 6);
-		}
-	}
+    static class StringUtil
+    {
+        public static string ToReadable(this Guid guid)
+        {
+            return guid.ToString().Substring(0, 6);
+        }
+    }
 }
